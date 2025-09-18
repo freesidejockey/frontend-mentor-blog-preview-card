ebd3dccd3003b33ea30f6b26a2fce05cb017f5c3
Freeside Jockey <freesidejockey@gmail.com>
2025-09-18 13:18:50 -0600

    feat: Improved to use box shadow vs separate box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,43 +3,39 @@ import Image from "next/image";
 export default function Home() {
   return (
     <article className="place-content-center h-screen">
-      <div className="relative">
-        <div className="absolute h-125 w-96 translate-x-29 translate-y-2 z-0 max-w-sm mx-auto bg-black rounded-xl"></div>{" "}
-        <div className="relative card z-10 border max-w-sm mx-auto bg-white rounded-xl">
-          <div>
-            <picture>
-              <img
-                src="/images/illustration-article.svg"
-                alt="illustration"
-                className="p-4 mb-1 mx-auto rounded-3xl"
-              />
-            </picture>
-          </div>
-          <div className="rounded-sm w-1/4 px-2 ml-6 mb-2 bg-yellow">
-            <p className="text-center font-extrabold text-xs py-1">Learning</p>
-          </div>
-          <p className="ml-6 pb-3 text-sm font-semibold">
-            Published 21 Dec 2023
-          </p>
-          <p className="ml-6 pb-3 text-2xl font-extrabold hover:text-yellow">
-            HTML & CSS foundations
-          </p>
-          <p className="ml-6 pb-6 font-semibold text-gray-500">
-            These languages are the backbone of every website, defining
-            structure, content, and presentation.
-          </p>
-          <div className="flex items-center ml-6 mb-6">
-            <picture>
-              <img
-                src="/images/image-avatar.webp"
-                alt="illustration"
-                className="w-8 h-8 "
-              />
-            </picture>
-            <p className="pl-3 font-extrabold">Greg Hooper</p>
-          </div>
+      <div className="border shadow-[8px_8px_black] max-w-sm mx-auto bg-white rounded-2xl">
+        <div>
+          <picture>
+            <img
+              src="/images/illustration-article.svg"
+              alt="illustration"
+              className="mb-4 mt-6 mx-auto rounded-2xl"
+            />
+          </picture>
+        </div>
+        <div className="rounded-sm w-1/4 px-2 ml-6 mb-3 bg-yellow">
+          <p className="text-center font-extrabold text-xs py-1">Learning</p>
+        </div>
+        <p className="ml-6 pb-3 text-sm font-semibold">Published 21 Dec 2023</p>
+        <p className="ml-6 pb-3 text-2xl font-extrabold hover:text-yellow">
+          HTML & CSS foundations
+        </p>
+        <p className="ml-6 pb-6 font-semibold text-gray-500">
+          These languages are the backbone of every website, defining structure,
+          content, and presentation.
+        </p>
+        <div className="flex items-center ml-6 mb-6">
+          <picture>
+            <img
+              src="/images/image-avatar.webp"
+              alt="illustration"
+              className="w-8 h-8 "
+            />
+          </picture>
+          <p className="pl-3 font-extrabold">Greg Hooper</p>
         </div>
       </div>
+      {/* </div> */}
       <div className="attribution mt-4">
         Challenge by{" "}
         <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
